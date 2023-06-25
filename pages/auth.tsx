888a4b4c78f592b1109877917965827aa51e42ae
Aleksey Bykhun <theisease@gmail.com>
2023-06-25 02:41:54 -0400

    add /auth page to capture token from chrome ext

---
+++ pages/auth.tsx
@@ -0,0 +1,45 @@
+import React from "react";
+import { GetServerSidePropsContext } from "next";
+
+import Script from "next/script";
+import { getToken } from "next-auth/jwt";
+
+import LoginButton from "../components/login-btn";
+import Profile from "../components/profile";
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+
+    const token = await getToken({
+        req: context.req,
+        secret: process.env.JWT_SECRET,
+    });
+
+    return {
+        props: {
+            token
+        },
+    };
+}
+
+export const Auth: React.FC<{ token: string }> = (props) => {
+    const { token } = props;
+
+    if (!token) return <>
+        <LoginButton />
+    </>
+
+    return <>
+        <LoginButton />
+
+        <h1>Logged in! You can now close this window.</h1>
+
+        <Script>
+            {`
+            window.localStorage.setItem('token', '${token}');
+            window.__WORLDCOIN_EMAIL_ACCESS_TOKEN__ = ${JSON.stringify(token)};
+            `}
+        </Script >
+    </>;
+}
+
+export default Auth;
