8e6edf9df76ab88ff5ddaaa7dea2c2c6192c7c63
emil <33105890+poolsar42@users.noreply.github.com>
2023-06-25 02:36:02 -0400

    wip

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -1,11 +1,11 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
-  // eslint-disable-page @next/next/no-sync-scripts 
+  // eslint-disable-page @next/next/no-sync-scripts
   return (
     <Html lang="en">
       <Head>
-        <script src="https://cdn.tailwindcss.com"></script>
+        <script async src="https://cdn.tailwindcss.com"></script>
       </Head>
       <body>
         <Main />
