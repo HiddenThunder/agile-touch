0361f6518d15cb272aa04107e6a01b348748eb0c
Aleksey Bykhun <theisease@gmail.com>
2023-06-25 05:51:27 -0400

    add favicon and readme

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,6 +6,33 @@ export default function Document() {
     <Html lang="en">
       <Head>
         <script async src="https://cdn.tailwindcss.com"></script>
+
+        {/* favicons are in favicon_io directory in different formats */}
+
+        <link rel="icon" href="/favicon_io/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/favicon_io/apple-touch-icon.png"
+        />
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="32x32"
+
+          href="/favicon_io/favicon-32x32.png"
+        />
+
+        <link
+          rel="icon"
+          type="image/png"
+          sizes="16x16"
+
+          href="/favicon_io/favicon-16x16.png"
+        />
+
+        <link rel="manifest" href="/favicon_io/site.webmanifest" />
+
       </Head>
       <body>
         <Main />
