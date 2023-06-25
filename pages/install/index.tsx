ef31d3d75740730c368a02d7273a9a4d9c121099
Aleksey Bykhun <theisease@gmail.com>
2023-06-25 07:25:22 -0400

    add redirect from /install

---
+++ pages/install/index.tsx
@@ -0,0 +1,17 @@
+import React, { useEffect } from "react";
+
+// redirect to https://github.com/worldcoin-email/extension
+
+export default function Index() {
+
+  useEffect(() => {
+    window.location.href = "https:///github.com/worldcoin-email/extension";
+  }, []);
+
+  return (
+    <div className="flex flex-col justify-center items-center h-screen">
+      <h1>Redirecting to
+        <a href="https:///github.com/worldcoin-email/extension">https:///github.com/worldcoin-email/extension</a>
+      </h1>
+    </div>
+}  
